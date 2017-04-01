681164c03798bc37862c1656e0ef871f6883fc78
Mark Bennett <mark@burmis.ca>
2017-04-01 09:19:17 -0600

    Setup the basic rendering pipeline.
    Hello Redux!

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	const initial_state = {
+		todos: [
+			{ 
+				description: "Build a FRP todo demo",
+				completed: false
+			}
+		]
+	};
+
+	const action$ = new Rx.Subject();
+
+	const store$ = action$.startWith(initial_state);
+
+	const reducer = (state, action) => {
+		// no-op
+		return state;
+	};
+
+	const renderer = (state) => {
+		console.log("RENDER STATE");
+		console.dir(state);
+
+		
+	};
+
+	store$.scan(reducer).subscribe(renderer)
 
 })(window);
