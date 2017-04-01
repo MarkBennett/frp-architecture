96ea0a28f396c2a6c6a1b2a5101aae1d3ebc4b91
Mark Bennett <mark@burmis.ca>
2017-04-01 14:46:49 -0600

    Get the reducer and renderer working with scan().
    This lets them modify the current and past state. It's useful if we want to build the new state from the old, or patch the existing DOM.

--- js/app.js
+++ js/app.js
@@ -13,20 +13,31 @@
 	// TODO: Replace this Subject with an Observable concatting together a bunch of action observables
 	const actions$ = new Rx.Subject();
 
-	const store$ = actions$.startWith(initial_state);
-
 	const reducer = (state, action) => {
+		console.log("REDUCING");
 		// no-op
 		return state;
 	};
 
-	const renderer = (state) => {
+	const store$ = actions$.scan(reducer, initial_state);
+
+	const renderer = (previous_dom, state) => {
 		console.log("RENDER STATE");
-		console.dir(state);
+		console.log(previous_dom);
+		console.log(state);
 
-
+		// TODO: snabbdom it here
+		return "NEW_DOM";
 	};
 
-	store$.scan(reducer).subscribe(renderer)
+	store$.scan(renderer, "DOM_NODE").subscribe((val) => { "no-op" });
+
+	const TODOS_CREATE = "TODOS_CREATE";
+	const TODO_CHANGE = "TODO_CHANGE";
+	const TODO_DESTROY = "TODO_DESTROY";
+
+	actions$.next({type: TODOS_CREATE});
+	actions$.next({type: TODO_CHANGE});
+	actions$.next({type: TODO_DESTROY});
 
 })(window);
